2b54de376c353df7dfc1cdfc5f9de6607e144b7b
AntLeszcz <antek.leszczynski@gmail.com>
2026-06-10 16:01:25 +0200

    updated app windows

--- App-windows/cmake_install.cmake
+++ App-windows/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/_Files/_Programieren/Studies/TKiK/TKiK-projekt/qt_app
+# Install script for directory: C:/Studia/kompilatory/TKiK-projekt/qt_app
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,6 +34,12 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2024.3.4/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2026.1.2/bin/mingw/bin/objdump.exe")
 endif()
 
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "C:/Studia/kompilatory/TKiK-projekt/cmake-build-debug/qt_app/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
